4c9778e53a4c82ceb82e215f9d6d603262da2665
baiyuntian <17605229265@163.com>
2026-01-07 15:50:46 +0800

    data

---
+++ data_collection.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+import csv
+from datetime import datetime
+sensor_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN)
+
+
+with open('moisture_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Timestamp", "Moisture_Level"])
+
+    try:
+        while True:
+            
+            moisture_value = GPIO.input(sensor_pin)
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+          
+            writer.writerow([timestamp, moisture_value])
+            print(f"Time: {timestamp}, Moisture: {moisture_value}")
+
+           
+            time.sleep(1800) 
+
+    except KeyboardInterrupt:
+        print("Stopping data collection.")
+    finally:
+        GPIO.cleanup()
